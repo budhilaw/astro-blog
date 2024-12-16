586511f06a3948e23cea11488a853fd1414b2129
Ericsson Budhilaw <ericsson.budhilaw@gmail.com>
2024-12-16 09:35:21 +0700

    chore(): add nix stuff

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ nodejs_22 pnpm ];
+        };
+
+      });
+}
